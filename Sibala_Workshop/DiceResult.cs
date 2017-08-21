4658b63dfe064d6606eecb24ccf37b7bf2b85061
Ethan Li <ethan.li@titansoft.com.sg>
2017-08-21 17:33:25 +0800

    add result class

---
+++ Sibala_Workshop/DiceResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibala_Workshop
+{
+    public enum DiceResultType
+    {
+        NoPoint,
+        HasPoint,
+        Same
+    }
+
+    public class DiceResult
+    {
+        public int Points { get; set; }
+        public DiceResultType Result { get; set; }
+    }
+}

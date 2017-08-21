e4988c432e518b1670d454bf1012a99e0681167a
Ethan Li <ethan.li@titansoft.com.sg>
2017-08-21 18:06:58 +0800

    add test

--- Sibala_Workshop/SiblalaTests.cs
+++ Sibala_Workshop/SiblalaTests.cs
@@ -11,6 +11,23 @@ namespace Sibala_Workshop
     public class SiblalaTests
     {
         [Test]
+        public void SameBiggerThanHasPoint()
+        {
+            var deceResult1 = new DiceResult
+            {
+                Result = DiceResultType.HasPoint,
+                Points = 10
+            };
+            var deceResult2 = new DiceResult
+            {
+                Result = DiceResultType.Same,
+                Points = 4
+            };
+            var compare = new DiceCompare();
+            Assert.AreEqual(-1, compare.Compare(deceResult1, deceResult2));
+        }
+
+        [Test]
         public void Test_Input_1234_return_nopoint()
         {
             Dice d = new Dice(1, 2, 3, 4);
